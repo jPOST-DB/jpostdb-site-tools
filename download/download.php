51928cc7760aa2167382919f07ef5132126ec0c2
moriya-dbcls <moriya@dbcls.rois.ac.jp>
2026-02-18 15:43:00 +0900

    update

--- download/download.php
+++ download/download.php
@@ -6,19 +6,19 @@
     $id = $array[1];
     $project = sprintf("JPST%06d", $id);
 
-    $file = './tmp/'.$dataset."_pipe";
-echo $project;
-    exec('sh ./script/gen_pipe.sh '.$file.' '.$dataset.' '.$project.' > /dev/null &');
+    $file_path = getenv('PWD')."/tmp/".$dataset."_pipe";
+
+    exec('sh ./script/gen_pipe.sh '.$file_path.' '.$dataset.' '.$project.' > /dev/null &');
     sleep(1);
 
-    if (file_exists($file)) {
+    if (file_exists($file_path)) {
       set_time_limit(0);
       header('Content-Description: File Transfer');
       header('Content-Type: application/octet-stream');
       header('Content-Disposition: attachment; filename="'.$dataset.'.tar"');
       header('Connection: close');
       ob_end_clean();
-      readfile($file);
+      readfile($file_path);
       exit();
     }
   }else{
